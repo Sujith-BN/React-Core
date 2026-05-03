0822275ad0db46f78410907a10aef54aeef6ac9b
Sujith BN <sujithbn01@gmail.com>
2026-05-04 00:35:20 +0530

    "Day-02"

--- READme.md
+++ READme.md
@@ -1,1 +1,8 @@
 #React core
+
+#Pracel
+-dev build
+-creates local server
+-HMR -hot module replacement
+-File wacthing algorithm written in c++
+-caching - faster builds
--- app.js
+++ app.js
@@ -1,17 +1,20 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
 
 const body = React.createElement("div",{
     id:"parent"
-},[React.createElement("div",{id:"child"},[
-    React.createElement("h1",{id:"heading1"},"Hi my name is Sujith BN"),
-    React.createElement("h1",{id:"heading2"},"Hi my name is Samarth BN")
+},[React.createElement("div",{id:"child",key:"c1"},[
+    React.createElement("h1",{id:"heading1",key:"he1"},"Hi my name is Sujith BN"),
+    React.createElement("h1",{id:"heading2",key:"he2"},"Hi my name is Samarth BN")
     ]
 ),
-    React.createElement("div",{id:"child2"},[
-    React.createElement("h1",{id:"heading1"},"Hi my name is Sujith BN"),
-    React.createElement("h1",{id:"heading2"},"Hi my name is Samarth BN")
+    React.createElement("div",{id:"child2",key:"c2"},[
+    React.createElement("h1",{id:"heading1",key:"he3"},"Hi my name is Sujith BN"),
+    React.createElement("h1",{id:"heading2",key:"he4"},"Hi my name is Samarth BN")
     ]
 )
 ])
+
 
 
 
